00af06c83af8338e41aedeb2dbf7378aceea1b02
BALKALOX <misiura.anton@lll.kpi.ua>
2023-02-05 17:02:25 +0200

    Create person with a  form

--- src/main/java/ua/com/anton/controllers/PeopleController.java
+++ src/main/java/ua/com/anton/controllers/PeopleController.java
@@ -3,10 +3,9 @@ package ua.com.anton.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import ua.com.anton.dao.PersonDAO;
+import ua.com.anton.models.Person;
 
 @Controller
 @RequestMapping("/people")
@@ -24,10 +23,19 @@ public class PeopleController {
         model.addAttribute("people", personDAO.index());
         return "people/index";
     }
-
     @GetMapping("/{id}")
     public String show(@PathVariable("id") int id, Model model) {
         model.addAttribute("person", personDAO.show(id));
         return "people/show";
+    }
+    @GetMapping("/new")
+    public String newPerson(Model model){
+        model.addAttribute("person",new Person());
+        return "people/new";
+    }
+    @PostMapping()
+    public String create(@ModelAttribute("person")Person person){
+        personDAO.save(person);
+        return "redirect:/people";
     }
 }
